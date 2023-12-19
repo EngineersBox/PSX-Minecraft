4aea38247cc695aa320eba4ecb10042ac4dab26e
EngineersBox <toastcraft.info@gmail.com>
2023-12-19 22:14:59 +1100

    Extracted render buffer into standalone header

---
+++ src/render/buffer.c
@@ -0,0 +1,24 @@
+#include "buffer.h"
+
+void setup_context(RenderContext *ctx, int w, int h, int r, int g, int b) {
+	// Place the two framebuffers vertically in VRAM.
+	SetDefDrawEnv(&(ctx->buffers[0].draw_env), 0, 0, w, h);
+	SetDefDispEnv(&(ctx->buffers[0].display_env), 0, 0, w, h);
+	SetDefDrawEnv(&(ctx->buffers[1].draw_env), 0, h, w, h);
+	SetDefDispEnv(&(ctx->buffers[1].display_env), 0, h, w, h);
+
+	// Set the default background color and enable auto-clearing.
+	setRGB0(&(ctx->buffers[0].draw_env), r, g, b);
+	setRGB0(&(ctx->buffers[1].draw_env), r, g, b);
+	ctx->buffers[0].draw_env.isbg = 1;
+	ctx->buffers[1].draw_env.isbg = 1;
+
+	// Initialize the first buffer and clear its OT so that it can be used for
+	// drawing.
+	ctx->active_buffer = 0;
+	ctx->next_packet = ctx->buffers[0].packet_buffer;
+	ClearOTagR(ctx->buffers[0].ordering_table, OT_LENGTH);
+
+	// Turn on the video output.
+	SetDispMask(1);
+}
