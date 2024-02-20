52b13adcbebbcef73c4ba2903fc3646eba42efae
EngineersBox <toastcraft.info@gmail.com>
2024-02-21 00:07:02 +1100

    Simplified clipping logic

--- src/ui/background.c
+++ src/ui/background.c
@@ -5,19 +5,19 @@
 #include "../resources/assets.h"
 #include "../blocks/block.h"
 
-void backgroundDraw(RenderContext* ctx, const int ot_entry, const int8_t u, const int8_t v) {
-    // BUG: For some reason using a polygon with width of the screen doesn't wrap textures
-    //      properly even with texture windowing.
-    // Left half of the screen
+void drawBackgroundHalf(RenderContext* ctx,
+                        const int ot_entry,
+                        const int8_t u, const int8_t v,
+                        const uint16_t x) {
     POLY_FT4* pol4 = (POLY_FT4*) allocatePrimitive(ctx, sizeof(POLY_FT4));
     setPolyFT4(pol4);
     // Set full screen vertex positions
     setXY4(
         pol4,
-        0, 0,
-        SCREEN_XRES / 2, 0,
-        0, SCREEN_YRES,
-        SCREEN_XRES / 2, SCREEN_YRES
+        x, 0,
+        (SCREEN_XRES >> 1 + x), 0,
+        x, SCREEN_YRES,
+        (SCREEN_XRES >> 1 + x), SCREEN_YRES
     );
     // Mid point grey as mask for additive texturing
     setRGB0(pol4, 0x80, 0x80, 0x80);
@@ -36,33 +36,68 @@ void backgroundDraw(RenderContext* ctx, const int ot_entry, const int8_t u, cons
     // Sort primitive to the ordering table
     uint32_t* ot_object = allocateOrderingTable(ctx, ot_entry);
     addPrim(ot_object, pol4);
-    // Right half of the screen
-    pol4 = (POLY_FT4*) allocatePrimitive(ctx, sizeof(POLY_FT4));
-    setPolyFT4(pol4);
-    // Set full screen vertex positions
-    setXY4(
-        pol4,
-        SCREEN_XRES / 2, 0,
-        SCREEN_XRES, 0,
-        SCREEN_XRES / 2, SCREEN_YRES,
-        SCREEN_XRES, SCREEN_YRES
-    );
-    // Mid point grey as mask for additive texturing
-    setRGB0(pol4, 0x80, 0x80, 0x80);
-    // Set texture coords and dimensions
-    setUVWH(
-        pol4,
-        u,
-        v,
-        SCREEN_XRES / 2,
-        SCREEN_YRES
-    );
-    // Bind texture page and colour look-up-table
-    pol4->tpage = texture->tpage;
-    pol4->clut = texture->clut;
-    // Sort primitive to the ordering table
-    ot_object = allocateOrderingTable(ctx, ot_entry);
-    addPrim(ot_object, pol4);
+}
+
+void backgroundDraw(RenderContext* ctx, const int ot_entry, const int8_t u, const int8_t v) {
+    // BUG: For some reason using a polygon with width of the screen doesn't wrap textures
+    //      properly even with texture windowing.
+    // Left half of the screen
+    drawBackgroundHalf(ctx, ot_entry, u, v, 0);
+    drawBackgroundHalf(ctx, ot_entry, u, v, SCREEN_XRES >> 1);
+    // POLY_FT4* pol4 = (POLY_FT4*) allocatePrimitive(ctx, sizeof(POLY_FT4));
+    // setPolyFT4(pol4);
+    // // Set full screen vertex positions
+    // setXY4(
+    //     pol4,
+    //     0, 0,
+    //     SCREEN_XRES >> 1, 0,
+    //     0, SCREEN_YRES,
+    //     SCREEN_XRES >> 1, SCREEN_YRES
+    // );
+    // // Mid point grey as mask for additive texturing
+    // setRGB0(pol4, 0x80, 0x80, 0x80);
+    // // Set texture coords and dimensions
+    // setUVWH(
+    //     pol4,
+    //     u,
+    //     v,
+    //     SCREEN_XRES / 2,
+    //     SCREEN_YRES
+    // );
+    // // Bind texture page and colour look-up-table
+    // const Texture* texture = &textures[TERRAIN_TEXTURES];
+    // pol4->tpage = texture->tpage;
+    // pol4->clut = texture->clut;
+    // // Sort primitive to the ordering table
+    // uint32_t* ot_object = allocateOrderingTable(ctx, ot_entry);
+    // addPrim(ot_object, pol4);
+    // // Right half of the screen
+    // pol4 = (POLY_FT4*) allocatePrimitive(ctx, sizeof(POLY_FT4));
+    // setPolyFT4(pol4);
+    // // Set full screen vertex positions
+    // setXY4(
+    //     pol4,
+    //     SCREEN_XRES / 2, 0,
+    //     SCREEN_XRES, 0,
+    //     SCREEN_XRES / 2, SCREEN_YRES,
+    //     SCREEN_XRES, SCREEN_YRES
+    // );
+    // // Mid point grey as mask for additive texturing
+    // setRGB0(pol4, 0x80, 0x80, 0x80);
+    // // Set texture coords and dimensions
+    // setUVWH(
+    //     pol4,
+    //     u,
+    //     v,
+    //     SCREEN_XRES / 2,
+    //     SCREEN_YRES
+    // );
+    // // Bind texture page and colour look-up-table
+    // pol4->tpage = texture->tpage;
+    // pol4->clut = texture->clut;
+    // // Sort primitive to the ordering table
+    // ot_object = allocateOrderingTable(ctx, ot_entry);
+    // addPrim(ot_object, pol4);
     // Advance to make another primitive
     // Bind a texture window to ensure wrapping across merged block face primitives
     DR_TWIN* ptwin = (DR_TWIN*) allocatePrimitive(ctx, sizeof(DR_TWIN));
@@ -80,7 +115,7 @@ void backgroundDraw(RenderContext* ctx, const int ot_entry, const int8_t u, cons
         tex_window.h
     );
     setTexWindow(ptwin, &tex_window);
-    ot_object = allocateOrderingTable(ctx, ot_entry);
+    uint32_t* ot_object = allocateOrderingTable(ctx, ot_entry);
     addPrim(ot_object, ptwin);
     renderClearConstraints(ctx);
 }
