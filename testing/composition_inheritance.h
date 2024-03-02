a74dcee2e8d328608441fc640fe45f167937aeb8
EngineersBox <toastcraft.info@gmail.com>
2024-03-02 21:35:39 +1100

    Implemented IBlock interface and restructured to support delcarative block registration

---
+++ testing/composition_inheritance.h
@@ -0,0 +1,78 @@
+/******************************************************************************
+
+Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define BLOCK_SIZE 50
+#define BLOCK_FACES 6
+#define BLOCK_TEXTURE_SIZE 16
+#define BLOCK_COUNT 256
+
+typedef uint8_t BlockID;
+
+typedef enum {
+    BLOCKTYPE_EMPTY = 0,
+    BLOCKTYPE_SOLID,
+    BLOCKTYPE_STAIR,
+    BLOCKTYPE_SLAB,
+    BLOCKTYPE_CROSS,
+    BLOCKTYPE_HASH
+} BlockType;
+
+typedef enum {
+    ORIENTATION_POS_X = 0,
+    ORIENTATION_NEG_X,
+    ORIENTATION_POS_Y,
+    ORIENTATION_NEG_Y,
+    ORIENTATION_POS_Z,
+    ORIENTATION_NEG_Z
+} Orientation;
+
+typedef struct {
+    BlockID id;
+    BlockType type;
+    Orientation orientation;
+    int16_t faceAttributes[BLOCK_FACES];
+    char* name;
+} Block;
+
+typedef struct {
+    Block block;
+    int8_t stone_type;
+    char* formatted;
+} Stone;
+
+int main() {
+    Stone stone = (Stone) {
+        .block = (Block) {
+            .id = 1,
+            .type = BLOCKTYPE_SOLID,
+            .orientation = ORIENTATION_POS_X,
+            .faceAttributes = { 1, 2, 3, 4, 5, 6  },
+            .name = "stone"
+        },
+        .stone_type = 3,
+        .formatted = "test"
+    };
+    Block* block = (Block*) &stone;
+    printf(
+        "id: %d, type: %d, orientation: %d, face attributes: [%d,%d,%d,%d,%d,%d], name: %s\n",
+        block->id,
+        block->type,
+        block->orientation,
+        block->faceAttributes[0],
+        block->faceAttributes[1],
+        block->faceAttributes[2],
+        block->faceAttributes[3],
+        block->faceAttributes[4],
+        block->faceAttributes[5],
+        block->name
+    );
+    return 0;
+}
