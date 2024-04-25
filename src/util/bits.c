26fe0a2d6081f326796285b5fc2b370c04be2aeb
EngineersBox <toastcraft.info@gmail.com>
2024-04-26 00:23:10 +1000

    Added inline notes for __clzSI2 impl compared to trailing_zeros

--- src/util/bits.c
+++ src/util/bits.c
@@ -55,3 +55,33 @@ u8 trailing_ones(u32 value) {
     }
     return c - !(value & 0x1);
 }
+
+// These functions (defines as macros in gcc/include/longlong.h are
+// used as the definition for __ctzSI2 in gcc/libgcc/libgcc2.c.
+// Specifically count_trailing_zeros. They seem to be the above
+// implementation in a more succinct form though unsure as to whether
+// the performance is better or not. If it is then we can replace
+// the use of trailing_zeros with __builtin_ctz.
+// Godbolt comparison: https://godbolt.org/z/ffqvYWsrr
+//
+// int count_leading_zeros(int x) {
+//     UWtype __xr = (x);
+//     UWtype __a;
+//     if (W_TYPE_SIZE <= 32) { // Never true since R3000 is 32 bit.. dunno why this isn't a preprocessor #if?
+//         __a = __xr < ((UWtype) 1<<2 * __BITS4)
+//             ? (__xr < ((UWtype) 1 << __BITS4) ? 0 : __BITS4)
+//             : (__xr < ((UWtype) 1 << 3 * __BITS4) ? 2 * __BITS4 : 3 * __BITS4);
+//     } else {
+//         for (__a = W_TYPE_SIZE - 8; __a > 0; __a -= 8)
+//             if (((__xr >> __a) & 0xff) != 0)
+//                 break;
+//     }
+//     return W_TYPE_SIZE - (__clz_tab[__xr >> __a] + __a);
+// }
+//
+// int count_trailing_zeros(int x) {
+//     UWtype __ctz_x = (x);
+//     UWtype __ctz_c;
+//     count_leading_zeros (__ctz_c, __ctz_x & -__ctz_x);
+//     return W_TYPE_SIZE - 1 - __ctz_c;
+// }
--- src/util/bits.h
+++ src/util/bits.h
@@ -8,4 +8,6 @@
 u8 trailing_zeros(u32 value);
 u8 trailing_ones(u32 value);
 
+
+
 #endif // PSX_MINECRAFT_BITS_H
