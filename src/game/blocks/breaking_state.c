6ef0d7443e4a229751ed4bcfa662460b013e263d
EngineersBox <toastcraft.info@gmail.com>
2024-06-25 18:37:52 +1000

    Fixed off-screen rendering for breaking texture

--- src/game/blocks/breaking_state.c
+++ src/game/blocks/breaking_state.c
@@ -1,5 +1,7 @@
 #include "breaking_state.h"
 
+#include <inline_c.h>
+#include <logging.h>
 #include <psxgpu.h>
 #include <psxgte.h>
 
@@ -16,10 +18,10 @@
 FWD_DECL IBlock* worldGetBlock(const World* world, const VECTOR* position);
 
 const RECT breaking_texture_offscreen = (RECT) {
-    .x = 784 >> 3,
-    .y = 0 >> 3,
-    .w = (16 * FACE_DIRECTION_COUNT) >> 3,
-    .h = 16 >> 3
+    .x = 784,
+    .y = 0,
+    .w = (16 * FACE_DIRECTION_COUNT),
+    .h = 16
 };
 
 bool canHarvestBlock(const ToolType block_tool_type,
@@ -123,37 +125,87 @@ void breakingStateUpdateRenderTarget(const BreakingState* state,
     const fixedi32 ticks_per_stage = state->ticks_per_stage;
     const fixedi32 ticks_so_far = state->ticks_so_far;
     const bool next_texture = ticks_so_far == 0 || (ticks_so_far / ticks_per_stage) > ((ticks_so_far - ONE) / ticks_per_stage);
-    if (state->block == NULL || !next_texture) {
+    if (state->block == NULL/* || !next_texture*/) {
         return;
     }
     // 2. For each visible face, blit the face texture to the offset position in the render target
-
     // Sort drawing primitives to reset drawing area to default (OT is in reverse order so that's
     // why this is here and not after the loop).
     DR_AREA* area = (DR_AREA*) allocatePrimitive(ctx, sizeof(DR_AREA));
     const DB* active = &ctx->db[ctx->active];
-    setDrawArea(area, &active->draw_env.clip);
-    u32* ot_entry = allocateOrderingTable(ctx, 0);
+    const DB* inactive = &ctx->db[1 - ctx->active];
+    const size_t ot_index = ORDERING_TABLE_LENGTH - 1;
+    setDrawArea(area, &inactive->draw_env.clip);
+    const u32* ot_entry = &active->ordering_table[ot_index];
     addPrim(ot_entry, area);
     DR_OFFSET* offset = (DR_OFFSET*) allocatePrimitive(ctx, sizeof(DR_OFFSET));
     setDrawOffset(
         offset,
-        active->draw_env.clip.x,
-        active->draw_env.clip.y
+        inactive->draw_env.clip.x,
+        inactive->draw_env.clip.y
     );
-    ot_entry = allocateOrderingTable(ctx, 0);
     addPrim(ot_entry, offset);
+    // Reset texture window that will be enabled for applying the breaking
+    // texture
+    RECT tex_window = (RECT) {0, 0, 0, 0};
+    DR_TWIN* ptwin = (DR_TWIN*) allocatePrimitive(ctx, sizeof(DR_TWIN));
+    setTexWindow(ptwin, &tex_window);
+    addPrim(ot_entry, ptwin);
+    // Sort a textured polygon that covers the entire offscreem TPage
+    // area and repeats the current stage of breaking texture over it
+    // with 50% blending. This saves needing to do multiple SPRT prims
+    // to cover the same area
+    const TextureAttributes face_attribute = (TextureAttributes) {
+        .u = (BLOCK_TEXTURE_SIZE * 2) + ((state->ticks_so_far / state->ticks_per_stage) * BLOCK_TEXTURE_SIZE),
+        .v = BLOCK_TEXTURE_SIZE * 14,
+        .w = BLOCK_TEXTURE_SIZE * FACE_DIRECTION_COUNT,
+        .h = BLOCK_TEXTURE_SIZE,
+        .tint = {0, 0, 0, 0}
+    };
+    const Texture* terrain_texture = &textures[ASSET_TEXTURES_TERRAIN_INDEX];
+    POLY_FT4* pol4 = (POLY_FT4*) allocatePrimitive(ctx, sizeof(POLY_FT4));
+    setPolyFT4(pol4);
+    setSemiTrans(pol4, 1);
+    // Note that coords are relative to the top left of the DR_AREA we set above
+    setXYWH(
+        pol4,
+        0,
+        0,
+        BLOCK_TEXTURE_SIZE * FACE_DIRECTION_COUNT,
+        BLOCK_TEXTURE_SIZE
+    );
+    setUVWH(
+        pol4,
+        face_attribute.u,
+        face_attribute.v,
+        face_attribute.w,
+        face_attribute.h
+    );
+    setRGB0(pol4, 0x80, 0x80, 0x80);
+    pol4->tpage = terrain_texture->tpage;
+    pol4->clut = terrain_texture->clut;
+    addPrim(ot_entry, pol4);
+    // Texture window to ensure wrapping across offscreen TPage
+    tex_window = (RECT) {
+        // All in units of 8 pixels, hence right shift by 3
+        .w = BLOCK_TEXTURE_SIZE >> 3,
+        .h = BLOCK_TEXTURE_SIZE >> 3,
+        .x = face_attribute.u >> 3,
+        .y = face_attribute.v >> 3
+    };
+    ptwin = (DR_TWIN*) allocatePrimitive(ctx, sizeof(DR_TWIN));
+    setTexWindow(ptwin, &tex_window);
+    addPrim(ot_entry, ptwin);
     const Block* block = VCAST_PTR(Block*, state->block);
     const TextureAttributes* attributes = block->face_attributes;
-    const Texture* terrain_texture = &textures[ASSET_TEXTURES_TERRAIN_INDEX];
     for (FaceDirection face_dir = 0; face_dir < FACE_DIRECTION_COUNT; face_dir++) {
         if (((state->visible_sides_bitset >> face_dir) & 0b1) == 0) {
             continue;
         }
         const TextureAttributes* attribute = &attributes[face_dir];
-        POLY_FT4* pol4 = (POLY_FT4*) allocatePrimitive(ctx, sizeof(POLY_FT4));
+        pol4 = (POLY_FT4*) allocatePrimitive(ctx, sizeof(POLY_FT4));
         setPolyFT4(pol4);
-        setSemiTrans(pol4, 1); // 0=B/2+F/2 1=B+F 2=B-F 3=B+F/4
+        setSemiTrans(pol4, 0);
         setXYWH(
             pol4,
             BLOCK_TEXTURE_SIZE * face_dir,
@@ -168,22 +220,24 @@ void breakingStateUpdateRenderTarget(const BreakingState* state,
             BLOCK_TEXTURE_SIZE,
             BLOCK_TEXTURE_SIZE
         );
+        setRGB0(
+            pol4,
+            0x80,
+            0x80,
+            0x80
+        );
         pol4->tpage = terrain_texture->tpage;
         pol4->clut = terrain_texture->clut;
-        ot_entry = allocateOrderingTable(ctx, 0);
         addPrim(ot_entry, pol4);
     }
-    // Reset texture window that will be enabled for applying the breaking
-    // texture
-    RECT tex_window = (RECT) {0, 0, 0, 0};
-    DR_TWIN* ptwin = (DR_TWIN*) allocatePrimitive(ctx, sizeof(DR_TWIN));
+    // Reset any previous windows
+    tex_window = (RECT) {0, 0, 0, 0};
+    ptwin = (DR_TWIN*) allocatePrimitive(ctx, sizeof(DR_TWIN));
     setTexWindow(ptwin, &tex_window);
-    ot_entry = allocateOrderingTable(ctx, 0);
     addPrim(ot_entry, ptwin);
     // Sort drawing bindings to ensure we target offscreen TPage area
     area = (DR_AREA*) allocatePrimitive(ctx, sizeof(DR_AREA));
     setDrawArea(area, &breaking_texture_offscreen);
-    ot_entry = allocateOrderingTable(ctx, 0);
     addPrim(ot_entry, area);
     offset = (DR_OFFSET*) allocatePrimitive(ctx, sizeof(DR_OFFSET));
     setDrawOffset(
@@ -191,52 +245,7 @@ void breakingStateUpdateRenderTarget(const BreakingState* state,
         breaking_texture_offscreen.x,
         breaking_texture_offscreen.y
     );
-    ot_entry = allocateOrderingTable(ctx, 0);
     addPrim(ot_entry, offset);
-    // Sort a textured polygon that covers the entire offscreem TPage
-    // area and repeats the current stage of breaking texture over it
-    // with 50% blending. This saves needing to do multiple SPRT prims
-    // to cover the same area
-    const TextureAttributes face_attribute = (TextureAttributes) {
-        .u = (BLOCK_TEXTURE_SIZE * 2) + ((state->ticks_so_far / state->ticks_per_stage) * BLOCK_TEXTURE_SIZE),
-        .v = BLOCK_TEXTURE_SIZE * 14,
-        .w = BLOCK_TEXTURE_SIZE,
-        .h = BLOCK_TEXTURE_SIZE,
-        .tint = {0, 0, 0, 0}
-    };
-    POLY_FT4* pol4 = (POLY_FT4*) allocatePrimitive(ctx, sizeof(POLY_FT4));
-    setPolyFT4(pol4);
-    setSemiTrans(pol4, 1); // 0=B/2+F/2 1=B+F 2=B-F 3=B+F/4
-    setXYWH(
-        pol4,
-        breaking_texture_offscreen.x,
-        breaking_texture_offscreen.y,
-        BLOCK_TEXTURE_SIZE * FACE_DIRECTION_COUNT,
-        BLOCK_TEXTURE_SIZE
-    );
-    setUVWH(
-        pol4,
-        face_attribute.u,
-        face_attribute.v,
-        face_attribute.w,
-        face_attribute.h
-    );
-    pol4->tpage = terrain_texture->tpage;
-    pol4->clut = terrain_texture->clut;
-    ot_entry = allocateOrderingTable(ctx, 0);
-    addPrim(ot_entry, pol4);
-    // Texture window to ensure wrapping across offscreen TPage
-    tex_window = (RECT){
-        // All in units of 8 pixels, hence right shift by 3
-        .w = BLOCK_TEXTURE_SIZE >> 3,
-        .h = BLOCK_TEXTURE_SIZE >> 3,
-        .x = face_attribute.u >> 3,
-        .y = face_attribute.v >> 3
-    };
-    ptwin = (DR_TWIN*) allocatePrimitive(ctx, sizeof(DR_TWIN));
-    setTexWindow(ptwin, &tex_window);
-    ot_entry = allocateOrderingTable(ctx, 0);
-    addPrim(ot_entry, ptwin);
     // 3. Sort sprite operations to render to offset positions and merge breaking texture with 50% blending.
     //    This must be sorted before any other chunk rendering takes place to ensure that the texture is
 
