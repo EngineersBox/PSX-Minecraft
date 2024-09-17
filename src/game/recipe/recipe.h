88248d78ee37b72b6bdc2da376b150ed887dc308
EngineersBox <toastcraft.info@gmail.com>
2024-09-17 23:34:54 +1000

    Added credits for design of recipe tree system

--- src/game/recipe/recipe.h
+++ src/game/recipe/recipe.h
@@ -31,6 +31,8 @@ typedef struct RecipeResult {
  *        3x2 area then you only need to include those nodes and ignore
  *        the 3x1 area that is unusesd). This format allows for recipes
  *        to be anywhere in the grid and still be matched.
+ * @see This is based on @link https://gamedev.stackexchange.com/a/21626
+ *      and https://www.reddit.com/r/C_Programming/comments/1b5y9r9/compiletime_initialization_of_arbitrarydepth/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
  */
 typedef struct RecipeNode {
     /**
