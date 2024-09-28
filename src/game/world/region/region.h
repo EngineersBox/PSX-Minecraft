4f164cee586674d1caadcd259f64a4174d8870f1
EngineersBox <toastcraft.info@gmail.com>
2024-09-28 22:24:04 +1000

    Testing chunk storage formats

--- src/game/world/region/region.h
+++ src/game/world/region/region.h
@@ -9,14 +9,64 @@
 #define REGION_WIDTH ((REGION_RADIUS * 2) + 1)
 #define REGION_CHUNKS_COUNT (REGION_WIDTH * REGION_WIDTH * REGION_WIDTH)
 
-typedef struct RegionChunk {
-    u8 generated_sub_chunks;
+typedef struct RegionHeader {
+	// Region location
+    struct {
+        i8 x;
+        i8 y;
+        i8 z;
+        u8 _pad;
+    } position;
+    // CRC32 checksum of region data
+    u32 crc;
+    // Original size of data in bytes
+    u32 file_size;
+    // Compressed size of data in bytes
+    u32 packed_size;
+    // Data offset
+    u32 offset;
+    // Pointer to the next region header;
+    u32 next;
+} RegionHeader;
 
-} RegionChunk;
+typedef struct ChunkHeader {
+	// Chunk location
+    struct {
+        i8 x;
+        i8 y;
+        i8 z;
+        u8 _pad;
+    } position;
+    // CRC32 checksum of chunk data
+    u32 crc;
+    // Original size of data in bytes
+    u32 file_size;
+    // Compressed size of data in bytes
+    u32 packed_size;
+    // Data offset
+    u32 offset;
+    // Pointer to the next chunk header;
+    u32 next;
+} ChunkHeader;
 
-typedef struct Region {
-    u8 generated_chunks;
-    RegionChunk chunks[REGION_CHUNKS_COUNT];
-} Region;
+typedef struct RegionArchiveMetadata {
+    struct {
+        i8 x;
+        i8 y;
+        i8 z;
+    } min;
+    struct {
+        i8 x;
+        i8 y;
+        i8 z;
+    } max;
+    // Data offset
+    u32 offset;
+} RegionArchiveMetadata;
+
+typedef struct RegionArchive {
+    u8 region_count;
+    RegionArchiveMetadata metadata[];
+} RegionArchive;
 
 #endif // _PSXMC__GAME_WORLD_REGION__REGION_H_
