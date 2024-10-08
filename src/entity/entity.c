17746dd4a8a2ed29dbc72368cb9726afba6fe885
EngineersBox <toastcraft.info@gmail.com>
2024-10-08 20:38:11 +1100

    Minimised padding in entity flags

--- src/entity/entity.c
+++ src/entity/entity.c
@@ -21,6 +21,9 @@ void IEntity_damage(VSelf, const i16 amount) {
     VSELF(Entity);
     // Bounded to [0,INT16_MAX] to ensure no integer over/underflow
     self->health = max(min((i16)self->health - amount, ENTITY_ABS_MAX_HEALTH), 0);
+    if (self->health == 0) {
+        self->flags.alive = false;
+    }
 }
 
 void iEntityKill(VSelf) ALIAS("IEntity_kill");
