d3089114dd6c9bfdf10377a28fe8b0ffdbc68d8d
EngineersBox <toastcraft.info@gmail.com>
2024-10-13 19:44:32 +1100

    Added utility CD read handler

---
+++ src/hardware/cd.c
@@ -0,0 +1,47 @@
+#include "cd.h"
+
+#include <stdlib.h>
+
+void* cdReadDataSync(const char* filename, CdlModeFlag mode) {
+    CdlFILE file;
+    if (!CdSearchFile(&file, filename)) {
+        printf("Unable to find assets.lzp\n");
+        return NULL;
+    }
+    int read_size;
+    switch (mode) {
+        case CdlModeSpeed:
+            read_size = 2048;
+            break;
+        case CdlModeSize:
+            read_size = 2340;
+            break;
+        default:
+            printf("[CD] Unimplemented read mode: %d\n", mode);
+            return NULL;
+    }
+    const int sector_count = (file.size / read_size) + 1;
+    int result = CdControlB(CdlSetloc, &file.pos, NULL);
+    if (result == 0) {
+        printf("[ASSETS] Previous pending command not finished\n");
+        return NULL;
+    } else if (result == -1) {
+        printf("[ASSETS] Missing required parameter for CdControlB\n");
+        return NULL;
+    }
+    u8* data = (u8*) malloc(sector_count * read_size);
+    if (!CdRead(sector_count, (void*) data, CdlModeSpeed)) {
+        printf("[ASSETS] Failed to read assets.lzp file from CD\n");
+        return NULL;
+    }
+    u8 res_buf = 0;
+    result = CdReadSync(0, &res_buf);
+    if (result == -1) {
+        printf("[ASSETS] CD read failed: %d\n", res_buf);
+        return NULL;
+    } else if (result == -2) {
+        printf("[ASSETS] CD read aborted\n");
+        return NULL;
+    }
+    return data;
+}
---
+++ src/hardware/cd.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef _PSXMC__HARDWARE__CD_H_
+#define _PSXMC__HARDWARE__CD_H_
+
+#include <psxcd.h>
+#include <stdio.h>
+#include <stddef.h>
+
+#include "../util/inttypes.h"
+#include "../logging/logging.h"
+
+void* cdReadDataSync(const char* filename, CdlModeFlag mode);
+
+#endif // _PSXMC__HARDWARE__CD_H_
