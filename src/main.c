505274c68c2e3392705056b9b28adad34d1dc59c
EngineersBox <toastcraft.info@gmail.com>
2024-12-15 17:27:34 +1100

    Restored main to PSXMC instead of testing

--- src/main.c
+++ src/main.c
@@ -1,112 +1,19 @@
-/*#include "core/engine.h"*/
-/*#include "core/app_logic.h"*/
-/*#include "game/minecraft.h"*/
-/**/
-/*static AppLogic app_logic;*/
-/*static Minecraft* minecraft;*/
-/**/
-/*int main() {*/
-/*    app_logic = DYN_LIT(Minecraft, AppLogic, {});*/
-/*    minecraft = (Minecraft*) &app_logic;*/
-/*    Engine engine = (Engine) {*/
-/*        .app_logic =  &app_logic,*/
-/*        .target_fps = 60,*/
-/*        .target_tps = 20*/
-/*    };*/
-/*    engineInit(&engine, NULL);*/
-/*    engineRun(&engine);*/
-/*    return 0;*/
-/*}*/
+#include "core/engine.h"
+#include "core/app_logic.h"
+#include "game/minecraft.h"
 
-#include <psxgpu.h>
-#include <psxgte.h>
-#include <psxcd.h>
-#include <inline_c.h>
-
-#include "logging/logging.h"
-#include "core/input/input.h"
-#include "render/render_context.h"
-#include "render/transforms.h"
-#include "resources/asset_indices.h"
-#include "resources/assets.h"
-#include "resources/texture.h"
-#include "util/inttypes.h"
-#include "render/font.h"
+static AppLogic app_logic;
+static Minecraft* minecraft;
 
 int main() {
-    RenderContext ctx = (RenderContext) {
-        .active = 0,
-        .db = {0},
-        .primitive = NULL,
-        .camera = NULL
+    app_logic = DYN_LIT(Minecraft, AppLogic, {});
+    minecraft = (Minecraft*) &app_logic;
+    Engine engine = (Engine) {
+        .app_logic =  &app_logic,
+        .target_fps = 60,
+        .target_tps = 20
     };
-    memset(&ctx, '\0', sizeof(RenderContext));
-    initRenderContext(&ctx);
-    inputInit(&input);
-    CdInit();
-    assetsLoad();
-    fontLoad();
-    FntLoad(960, 256);
-    FntOpen(0, 8, 320, 216, 0, 160);
-    const u32 u = 11 * 16;
-    const u32 v = 2 * 16;
-    const u32 w = 32;
-    const u32 h = 32;
-    RECT tex_window = textureWindowCreate(16, 16, u, v);
-    DEBUG_LOG(
-        "Window [Mask X: %d] [Mask Y: %d] [Offset X: %d] [Offset Y: %d]\n",
-        tex_window.w,
-        tex_window.h,
-        tex_window.x,
-        tex_window.y
-    );
-    const Texture* texture = &textures[ASSET_TEXTURE__STATIC__TERRAIN];
-    int ticks = 0;
-    i16* target = &tex_window.w;
-    char* selected = "Selected: Mask X\n";
-    while (true) {
-        if (ticks == 0) {
-            if (isPressed(input.pad, PAD_TRIANGLE)) {
-                target = &tex_window.w;
-                selected = "Selected: Mask X\n";
-            } else if (isPressed(input.pad, PAD_CIRCLE)) {
-                target = &tex_window.h;
-                selected = "Selected: Mask Y\n";
-            } else if (isPressed(input.pad, PAD_CROSS)) {
-                target = &tex_window.x;
-                selected = "Selected: Offset X\n";
-            } else if (isPressed(input.pad, PAD_SQUARE)) {
-                target = &tex_window.y;
-                selected = "Selected: Offset Y\n";
-            }
-            if (isPressed(input.pad, PAD_UP)) {
-                if (*target < 0b11111) (*target)++;
-            } else if (isPressed(input.pad, PAD_DOWN)) {
-                if (*target > 0b00000) (*target)--;
-            }
-        }
-        FntPrint(0, selected);
-        FntPrint(0, "Mask X: %d = 0b" INT8_BIN_PATTERN "\n", tex_window.w, INT8_BIN_LAYOUT(tex_window.w));
-        FntPrint(0, "Mask Y: %d = 0b" INT8_BIN_PATTERN "\n", tex_window.h, INT8_BIN_LAYOUT(tex_window.h));
-        FntPrint(0, "Offset X: %d = 0b" INT8_BIN_PATTERN "\n", tex_window.x, INT8_BIN_LAYOUT(tex_window.x));
-        FntPrint(0, "Offset Y: %d = 0b" INT8_BIN_PATTERN "\n", tex_window.y, INT8_BIN_LAYOUT(tex_window.y));
-        FntFlush(0);
-        renderClearConstraints(&ctx);
-        POLY_FT4* pol4 = (POLY_FT4*) allocatePrimitive(&ctx, sizeof(POLY_FT4));
-        setPolyFT4(pol4);
-        setXYWH(pol4, CENTRE_X - 16, CENTRE_Y - 16, 32, 32);
-        setUVWH(pol4, u, v, w, h);
-        setRGB0(pol4, 0x7F, 0x7F, 0x7F);
-        pol4->tpage = texture->tpage;
-        pol4->clut = texture->clut;
-        u32* ot_object = allocateOrderingTable(&ctx, 1);
-        addPrim(ot_object, pol4);
-        DR_TWIN* ptwin = (DR_TWIN*) allocatePrimitive(&ctx, sizeof(DR_TWIN));
-        setTexWindow(ptwin, &tex_window);
-        addPrim(ot_object, ptwin);
-        renderClearConstraints(&ctx);
-        swapBuffers(&ctx);
-        ticks = (ticks + 1) % 5;
-    }
+    engineInit(&engine, NULL);
+    engineRun(&engine);
     return 0;
 }
--- testing/texture_window.c
+++ testing/texture_window.c
@@ -1,78 +1,112 @@
-#include <stdio.h>
-#include <stdint.h>
+/*#include "core/engine.h"*/
+/*#include "core/app_logic.h"*/
+/*#include "game/minecraft.h"*/
+/**/
+/*static AppLogic app_logic;*/
+/*static Minecraft* minecraft;*/
+/**/
+/*int main() {*/
+/*    app_logic = DYN_LIT(Minecraft, AppLogic, {});*/
+/*    minecraft = (Minecraft*) &app_logic;*/
+/*    Engine engine = (Engine) {*/
+/*        .app_logic =  &app_logic,*/
+/*        .target_fps = 60,*/
+/*        .target_tps = 20*/
+/*    };*/
+/*    engineInit(&engine, NULL);*/
+/*    engineRun(&engine);*/
+/*    return 0;*/
+/*}*/
 
-typedef uint8_t u8;
-typedef int8_t i8;
+#include <psxgpu.h>
+#include <psxgte.h>
+#include <psxcd.h>
+#include <inline_c.h>
 
-typedef uint16_t u16;
-typedef int16_t i16;
-
-typedef uint32_t u32;
-typedef int32_t i32;
-
-typedef uint64_t u64;
-typedef int64_t i64;
-
-typedef struct {
-    i16 x;
-    i16 y;
-    i16 w;
-    i16 h;
-} TWindow;
-
-typedef struct {
-    i16 x0;
-    i16 y0;
-    i16 x1;
-    i16 y1;
-} MTexWin;
+#include "logging/logging.h"
+#include "core/input/input.h"
+#include "render/render_context.h"
+#include "render/transforms.h"
+#include "resources/asset_indices.h"
+#include "resources/assets.h"
+#include "resources/texture.h"
+#include "util/inttypes.h"
+#include "render/font.h"
 
 int main() {
-    MTexWin m_textureWindow = {0};
-    TWindow _prim = {
-        .w = 16 >> 3,
-        .h = 16 >> 3,
-        .x = 0 >> 3,
-        .y = 16 >> 3
+    RenderContext ctx = (RenderContext) {
+        .active = 0,
+        .db = {0},
+        .primitive = NULL,
+        .camera = NULL
     };
-    TWindow* prim = &_prim;
-    // Texture window size is determined by the least bit set of the relevant 5 bits
-    if (prim->y & 0x01) {
-        m_textureWindow.y1 = 8;  // xxxx1
-    } else if (prim->y & 0x02) {
-        m_textureWindow.y1 = 16;  // xxx10
-    } else if (prim->y & 0x04) {
-        m_textureWindow.y1 = 32;  // xx100
-    } else if (prim->y & 0x08) {
-        m_textureWindow.y1 = 64;  // x1000
-    } else if (prim->y & 0x10) {
-        m_textureWindow.y1 = 128;  // 10000
-    } else {
-        m_textureWindow.y1 = 256;  // 00000
+    memset(&ctx, '\0', sizeof(RenderContext));
+    initRenderContext(&ctx);
+    inputInit(&input);
+    CdInit();
+    assetsLoad();
+    fontLoad();
+    FntLoad(960, 256);
+    FntOpen(0, 8, 320, 216, 0, 160);
+    const u32 u = 11 * 16;
+    const u32 v = 2 * 16;
+    const u32 w = 32;
+    const u32 h = 32;
+    RECT tex_window = textureWindowCreate(16, 16, u, v);
+    DEBUG_LOG(
+        "Window [Mask X: %d] [Mask Y: %d] [Offset X: %d] [Offset Y: %d]\n",
+        tex_window.w,
+        tex_window.h,
+        tex_window.x,
+        tex_window.y
+    );
+    const Texture* texture = &textures[ASSET_TEXTURE__STATIC__TERRAIN];
+    int ticks = 0;
+    i16* target = &tex_window.w;
+    char* selected = "Selected: Mask X\n";
+    while (true) {
+        if (ticks == 0) {
+            if (isPressed(input.pad, PAD_TRIANGLE)) {
+                target = &tex_window.w;
+                selected = "Selected: Mask X\n";
+            } else if (isPressed(input.pad, PAD_CIRCLE)) {
+                target = &tex_window.h;
+                selected = "Selected: Mask Y\n";
+            } else if (isPressed(input.pad, PAD_CROSS)) {
+                target = &tex_window.x;
+                selected = "Selected: Offset X\n";
+            } else if (isPressed(input.pad, PAD_SQUARE)) {
+                target = &tex_window.y;
+                selected = "Selected: Offset Y\n";
+            }
+            if (isPressed(input.pad, PAD_UP)) {
+                if (*target < 0b11111) (*target)++;
+            } else if (isPressed(input.pad, PAD_DOWN)) {
+                if (*target > 0b00000) (*target)--;
+            }
+        }
+        FntPrint(0, selected);
+        FntPrint(0, "Mask X: %d = 0b" INT8_BIN_PATTERN "\n", tex_window.w, INT8_BIN_LAYOUT(tex_window.w));
+        FntPrint(0, "Mask Y: %d = 0b" INT8_BIN_PATTERN "\n", tex_window.h, INT8_BIN_LAYOUT(tex_window.h));
+        FntPrint(0, "Offset X: %d = 0b" INT8_BIN_PATTERN "\n", tex_window.x, INT8_BIN_LAYOUT(tex_window.x));
+        FntPrint(0, "Offset Y: %d = 0b" INT8_BIN_PATTERN "\n", tex_window.y, INT8_BIN_LAYOUT(tex_window.y));
+        FntFlush(0);
+        renderClearConstraints(&ctx);
+        POLY_FT4* pol4 = (POLY_FT4*) allocatePrimitive(&ctx, sizeof(POLY_FT4));
+        setPolyFT4(pol4);
+        setXYWH(pol4, CENTRE_X - 16, CENTRE_Y - 16, 32, 32);
+        setUVWH(pol4, u, v, w, h);
+        setRGB0(pol4, 0x7F, 0x7F, 0x7F);
+        pol4->tpage = texture->tpage;
+        pol4->clut = texture->clut;
+        u32* ot_object = allocateOrderingTable(&ctx, 1);
+        addPrim(ot_object, pol4);
+        DR_TWIN* ptwin = (DR_TWIN*) allocatePrimitive(&ctx, sizeof(DR_TWIN));
+        setTexWindow(ptwin, &tex_window);
+        addPrim(ot_object, ptwin);
+        renderClearConstraints(&ctx);
+        swapBuffers(&ctx);
+        ticks = (ticks + 1) % 5;
     }
-
-    if (prim->x & 0x01) {
-        m_textureWindow.x1 = 8;  // xxxx1
-    } else if (prim->x & 0x02) {
-        m_textureWindow.x1 = 16;  // xxx10
-    } else if (prim->x & 0x04) {
-        m_textureWindow.x1 = 32;  // xx100
-    } else if (prim->x & 0x08) {
-        m_textureWindow.x1 = 64;  // x1000
-    } else if (prim->x & 0x10) {
-        m_textureWindow.x1 = 128;  // 10000
-    } else {
-        m_textureWindow.x1 = 256;  // 00000
-    }
-
-    // Re-calculate the bit field, because we can't trust what is passed in the data
-    u32 YAlign = (uint32_t)(32 - (m_textureWindow.y1 >> 3));
-    u32 XAlign = (uint32_t)(32 - (m_textureWindow.x1 >> 3));
-
-    // Absolute position of the start of the texture window
-    m_textureWindow.y0 = (int16_t)((prim->h & YAlign) << 3);
-    m_textureWindow.x0 = (int16_t)((prim->w & XAlign) << 3);
-    printf("x0: %d, y0: %d\nx1: %d, y1: %d\n", m_textureWindow.x0, m_textureWindow.y0, m_textureWindow.x1, m_textureWindow.y1);
-
     return 0;
 }
