2a4fc66b4d330dda2ab615e9be760c9eca3e2e65
EngineersBox <35655145+EngineersBox@users.noreply.github.com>
2025-10-22 22:16:28 +1100

    Added TODO for items ctor and attr array const init
    Signed-off-by: EngineersBox <35655145+EngineersBox@users.noreply.github.com>

--- src/game/items/items.c
+++ src/game/items/items.c
@@ -3,6 +3,12 @@
 #include "item.h"
 #include "item_id.h"
 
+// TODO: I think both arrays should be constant and initialised
+//       at compile time. We know all of the ctor's ahead of time
+//       so there is no point in leaving it to runtime init with
+//       the helper macros. A bit more dev pain, but worth the
+//       cost to allow the compiler to inline all usages of array 
+//       elements at compile time.
 ItemConstructor item_constructors[ITEM_COUNT] = {0};
 ItemAttributes item_attributes[ITEM_COUNT] = {0};
 
