9f1a6ab86b6f200be57c9a6712ebccdff5a9e380
EngineersBox <35655145+EngineersBox@users.noreply.github.com>
2025-10-29 17:38:18 +1100

    Fixed closed handling of dialog behaviour
    Signed-off-by: EngineersBox <35655145+EngineersBox@users.noreply.github.com>

--- tools/menu_designer/src/control/file_dialog.py
+++ tools/menu_designer/src/control/file_dialog.py
@@ -1,47 +1,13 @@
 import pygame, pygame_gui
 from pathlib import Path
-from typing import Callable, Optional, Set, Union
+from typing import Callable, Optional
 from src.control.base import ControlBase
 
 type ControlFileDialogSelectedCommand = Callable[[Path], None]
 type ControlFileDialogClosedCommand = Callable[[], None]
 
-class UIHidingFileDialog(pygame_gui.windows.UIFileDialog):
-    closed_command: ControlFileDialogClosedCommand
-
-    def __init__(
-        self,
-        rect: pygame.Rect,
-        manager: Optional[pygame_gui.UIManager],
-        closed_command: ControlFileDialogClosedCommand,
-        window_title: str = "pygame-gui.file_dialog_title_bar",
-        allowed_suffixes: Optional[Set[str]] = None,
-        initial_file_path: Optional[str] = None,
-        object_id: Union[pygame_gui.core.ObjectID, str] = pygame_gui.core.ObjectID("#file_dialog", None),
-        allow_existing_files_only: bool = False,
-        allow_picking_directories: bool = False,
-        visible: int = 1,
-        always_on_top: bool = False,
-    ):
-        super().__init__(
-            rect,
-            manager,
-            window_title,
-            allowed_suffixes,
-            initial_file_path,
-            object_id,
-            allow_existing_files_only,
-            allow_picking_directories,
-            visible,
-            always_on_top
-        )
-        self.closed_command = closed_command
-
-    def on_close_window_button_pressed(self):
-        self.closed_command()
-
 class ControlFileDialog(ControlBase):
-    file_dialog: Optional[UIHidingFileDialog]
+    file_dialog: Optional[pygame_gui.windows.UIFileDialog]
     selected_command: ControlFileDialogSelectedCommand
     closed_command: ControlFileDialogClosedCommand
 
@@ -72,10 +38,9 @@ class ControlFileDialog(ControlBase):
         self.closed_command = closed_command
 
     def open(self):
-        self.file_dialog = UIHidingFileDialog(
+        self.file_dialog = pygame_gui.windows.UIFileDialog(
             rect=self.rect,
             manager=self._manager,
-            closed_command=self.closed_command,
             window_title=self._title,
             allow_existing_files_only=True,
             allow_picking_directories=False,
