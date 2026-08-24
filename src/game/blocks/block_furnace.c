a739e2dc56e2ff631b8ecae721c95cf2fc64f28c
EngineersBox <35655145+EngineersBox@users.noreply.github.com>
2026-08-24 23:01:26 +1000

    Added tooltip and basic bg + slot render handlers to furnace
    Signed-off-by: EngineersBox <35655145+EngineersBox@users.noreply.github.com>

--- src/game/blocks/block_furnace.c
+++ src/game/blocks/block_furnace.c
@@ -3,8 +3,14 @@
 #include "block.h"
 #include "block_id.h"
 #include "../items/blocks/item_block_furnace.h"
+#include "../items/items.h"
+#include "../gui/inventory.h"
+#include "../gui/tooltip.h"
+#include "../gui/utils.h"
+#include "../recipe/crafting.h"
 #include "../../core/input/input.h"
 #include "../../logging/logging.h"
+#include "../../ui/components/cursor.h"
 #include "../../util/interface99_extensions.h"
 
 static Texture furnace_texture = {0};
@@ -25,6 +31,9 @@ static InputHandlerVTable furnaceBlockInputHandlerVTable = {
     .input_handler = furnaceBlockInputHandler,
     .input_handler_destroy = NULL
 };
+
+static u8 ingredient_consume_sizes[slotGroupSize(FURNACE_INPUT) + slotGroupSize(FURNACE_FUEL)] = {0};
+static RECIPE_PATTERN(pattern, slotGroupSize(FURNACE_INPUT) + slotGroupSize(FURNACE_FUEL)) = {0};
 
 DEFN_BLOCK_CONSTRUCTOR_IMPL_STATEFUL(furnace) {
     TODO("Constructor for furnace block");
@@ -81,9 +90,95 @@ bool FurnaceBlock_useAction(VSelf) {
         FURNACE_TEXTURE_WIDTH,
         FURNACE_TEXTURE_HEIGHT
     );
-    UNIMPLEMENTED();
-    return false;
+    block_render_ui_context.background.component = (UIComponent) {
+        .position = vec2_i16(
+            CENTRE_X - (FURNACE_TEXTURE_WIDTH >> 1),
+            CENTRE_Y - (FURNACE_TEXTURE_HEIGHT >> 1)
+        ),
+        .dimensions = vec2_i16(
+            FURNACE_TEXTURE_WIDTH,
+            FURNACE_TEXTURE_HEIGHT
+        )
+    };
+    block_render_ui_context.background.tint = vec3_rgb(0x80, 0x80, 0x80);
+    block_render_ui_context.background.ot_entry_index = 1;
+    return BLOCK_USE_ACTION_CONSUMED;
+}
+
+void furnaceRenderTooltip(RenderContext* ctx) {
+    if (slotGroupIntersect(FURNACE_INPUT, &cursor.component.position)) {
+        const Slot* slot = &furnace_slots[slotGroupCursorSlot(
+            FURNACE_INPUT,
+            &cursor.component.position
+        )];
+        if (slot->data.item != NULL) {
+            const Item* item = VCAST_PTR(Item*, slot->data.item);
+            toolTipRender(ctx, itemGetName(item->id));
+        }
+    }
+    if (slotGroupIntersect(FURNACE_FUEL, &cursor.component.position)) {
+        const Slot* slot = &furnace_slots[slotGroupCursorSlot(
+            FURNACE_FUEL,
+            &cursor.component.position
+        )];
+        if (slot->data.item != NULL) {
+            const Item* item = VCAST_PTR(Item*, slot->data.item);
+            toolTipRender(ctx, itemGetName(item->id));
+        }
+    }
+    if (slotGroupIntersect(FURNACE_OUTPUT, &cursor.component.position)) {
+        const Slot* slot = &furnace_slots[slotGroupCursorSlot(
+            FURNACE_OUTPUT,
+            &cursor.component.position
+        )];
+        if (slot->data.item != NULL) {
+            const Item* item = VCAST_PTR(Item*, slot->data.item);
+            toolTipRender(ctx, itemGetName(item->id));
+        }
+    }
 }
 
 void furnaceBlockRenderUI(RenderContext* ctx, Transforms* transforms) {
+    uiCursorRender(&cursor, ctx, transforms);
+    if (quadIntersectLiteral(
+        &cursor.component.position,
+        CENTRE_X - (FURNACE_TEXTURE_WIDTH >> 1),
+        CENTRE_Y - (FURNACE_TEXTURE_HEIGHT >> 1),
+        FURNACE_TEXTURE_WIDTH,
+        FURNACE_TEXTURE_HEIGHT
+    )) {
+        furnaceRenderTooltip(ctx);
+    }
+    inventoryRenderSlots(
+        VCAST_PTR(const Inventory*, block_input_handler_context.inventory),
+        INVENTORY_SLOT_GROUP_MAIN | INVENTORY_SLOT_GROUP_HOTBAR,
+        ctx,
+        transforms
+    );
+    const Slot* slot = &furnace_slots[slotGroupIndexOffset(FURNACE_INPUT)];
+    if (slot->data.item != NULL) {
+        Item* item = VCAST_PTR(Item*, slot->data.item);
+        item->position.vx = slotGroupScreenPosition(FURNACE_INPUT, X, 0);
+        item->position.vy = slotGroupScreenPosition(FURNACE_INPUT, Y, 0);
+        VCALL_SUPER(*slot->data.item, Renderable, renderInventory, ctx, transforms);
+    }
+    slot = &furnace_slots[slotGroupIndexOffset(FURNACE_FUEL)];
+    if (slot->data.item != NULL) {
+        Item* item = VCAST_PTR(Item*, slot->data.item);
+        item->position.vx = slotGroupScreenPosition(FURNACE_FUEL, X, 0);
+        item->position.vy = slotGroupScreenPosition(FURNACE_FUEL, Y, 0);
+        VCALL_SUPER(*slot->data.item, Renderable, renderInventory, ctx, transforms);
+    }
+    slot = &furnace_slots[slotGroupIndexOffset(FURNACE_OUTPUT)];
+    if (slot->data.item != NULL) {
+        Item* item = VCAST_PTR(Item*, slot->data.item);
+        item->position.vx = slotGroupScreenPosition(FURNACE_OUTPUT, X, 0);
+        item->position.vy = slotGroupScreenPosition(FURNACE_OUTPUT, Y, 0);
+        VCALL_SUPER(*slot->data.item, Renderable, renderInventory, ctx, transforms);
+    }
+    uiBackgroundRender(
+        &block_render_ui_context.background,
+        ctx,
+        transforms
+    );
 }
